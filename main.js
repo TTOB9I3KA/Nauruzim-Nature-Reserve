56da461e22230c974b870c206f6a80eb240f2c40
Nikita <donssmit@mail.ru>
2024-05-22 18:01:13 +0500

    animation test

--- main.js
+++ main.js
@@ -8,3 +8,36 @@
 //     currentIndex = (currentIndex + 1) % images.length;
 //     image.src = images[currentIndex];
 // });
+
+document.addEventListener("DOMContentLoaded", function(event) {
+	var textElement = document.querySelector(".welcome-text-animation");
+	var buttonElement = document.querySelector(".welcome-button-animation");
+
+	var options = {
+		threshold: 0.5
+	};
+
+	var textObserver = new IntersectionObserver(function(entries, observer) {
+		entries.forEach(function(entry) {
+			if (entry.isIntersecting) {
+				entry.target.classList.add("slideInFromTop");
+			} else {
+				entry.target.classList.remove("slideInFromTop");
+			}
+		});
+	}, options);
+
+	textObserver.observe(textElement);
+
+	var buttonObserver = new IntersectionObserver(function(entries, observer) {
+		entries.forEach(function(entry) {
+			if (entry.isIntersecting) {
+				entry.target.classList.add("slideInFromBottom");
+			} else {
+				entry.target.classList.remove("slideInFromBottom");
+			}
+		});
+	}, options);
+
+	buttonObserver.observe(buttonElement);
+});

fe02b9985309987ab34121afd35e58eafe6a8613
Nikita <donssmit@mail.ru>
2024-05-22 16:10:18 +0500

    changes

--- main.js
+++ main.js
@@ -1,12 +1,10 @@
-const images = ['./image/заповедник.jpg', './image/зап.svg', './image/kozel1.svg', './image/горы.svg'];
-let currentIndex = 0;
-const image = document.querySelector('.img');
+// const images = ['./image/заповедник.jpg', './image/зап.svg', './image/kozel1.svg', './image/горы.svg'];
+// let currentIndex = 0;
+// const image = document.querySelector('.img');
 
 
-image.addEventListener('click', () => {
-	console.log('sda')
-    currentIndex = (currentIndex + 1) % images.length;
-    image.src = images[currentIndex];
-});
-
-   
+// image.addEventListener('click', () => {
+// 	console.log('sda')
+//     currentIndex = (currentIndex + 1) % images.length;
+//     image.src = images[currentIndex];
+// });

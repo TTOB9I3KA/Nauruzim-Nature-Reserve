6435e771c364eac7109ea6b3a72f536af70f230e
JerryImMouse <toperkas@gmail.com>
2024-05-23 19:30:36 +0500

    front

--- public/contents/flora/flora.js
+++ public/contents/flora/flora.js
@@ -1,15 +1,57 @@
-function scrollLeft() {
-	const floraContainer = document.getElementById('flora');
-	floraContainer.scrollBy({
-			left: -400, // Adjust the scroll distance as needed
-			behavior: 'smooth'
-	});
-}
+const leftButton = document.querySelector('.scroll-btn-left');
+const rightButton = document.querySelector('.scroll-btn-right');
 
-function scrollRight() {
-	const floraContainer = document.getElementById('flora');
-	floraContainer.scrollBy({
-			left: 400, // Adjust the scroll distance as needed
-			behavior: 'smooth'
-	});
-}
+leftButton.addEventListener('click', function() {
+    const floraContainer = document.querySelector('.flora');
+    floraContainer.scrollBy({
+        left: -500,
+        behavior: 'smooth'
+    });
+});
+
+rightButton.addEventListener('click', function() {
+    const floraContainer = document.querySelector('.flora');
+    floraContainer.scrollBy({
+        left: 500,
+        behavior: 'smooth'
+    });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function scrollLeft() {
+// 	console.log('GGG')
+//     const floraContainer = document.getElementById('flora');
+//     floraContainer.scrollLeft({
+//         left: -500, // Adjust the scroll distance as needed
+//         behavior: 'smooth'
+//     });
+// }
+
+// function scrollRight() {
+// 	console.log('JJJ')
+//     const floraContainer = document.getElementById('flora');
+//     floraContainer.scrollBy({
+//         left: 500, // Adjust the scroll distance as needed
+//         behavior: 'smooth'
+//     });
+// }
